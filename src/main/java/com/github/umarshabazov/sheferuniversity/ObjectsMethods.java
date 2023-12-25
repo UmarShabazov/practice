ed8d5f58620bb754954d2b28b347ed0ebc0d2e68
Umar Shabazov <malik95i@yandex.ru>
2023-12-25 20:55:24 +0300

    OOP concepts (lesson 1: toString)

---
+++ src/main/java/com/github/umarshabazov/sheferuniversity/ObjectsMethods.java
@@ -0,0 +1,16 @@
+package com.github.umarshabazov.sheferuniversity;
+
+public class ObjectsMethods {
+    public static void main(String[] args) {
+
+        Student ivanov = new Student();
+
+        ivanov.name = "Sasha";
+        ivanov.grade = "7";
+
+
+        System.out.println(ivanov);
+
+    }
+}
+
---
+++ src/main/java/com/github/umarshabazov/sheferuniversity/Student.java
@@ -0,0 +1,12 @@
+package com.github.umarshabazov.sheferuniversity;
+
+public class Student {
+
+
+    String name;
+    String grade;
+
+    public String toString () {
+        return "Hello, my name is " + name + ". I am in " + grade + " grade." ;
+    }
+}
